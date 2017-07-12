660fbab5d0d4530f5b31a140d14193331b81bb24
Patrick O'Brien <obrienp@outlook.com>
2017-07-12 17:49:25 -0500

    factored out average functions

--- server.js
+++ server.js
@@ -109,14 +109,9 @@ function calculateWod1() {
     return a - b;
   }).reverse();
 
-  var wod1Total = 0;
-  for(var i = 0; i < wod1.length; ++i) {
-    wod1Total += +wod1[i];
-  }
-  
   return 'Workout 1 (' + wod1.length + ' submissions):\n' +
-         '  Team Average: ' + Math.round(+wod1Total / +wod1.length) + ' pounds\n' +
-         '  Team Median: ' + Math.round(median(wod1)) + '\n' +
+         '  Team Average: ' + average(wod1) + ' pounds\n' +
+         '  Team Median: ' + median(wod1) + '\n' +
          '  Team High: ' + wod1[0] + '\n' +
          '  Team Low: ' + wod1[wod1.length - 1] + '\n\n';
 }
@@ -125,16 +120,11 @@ function calculateWod2() {
   wod2.sort(function(a, b) {
     return a - b;
   }).reverse();
-
-  var wod2Total = 0;
-  for(var i = 0; i < wod2.length; ++i) {
-    wod2Total += +wod2[i];
-  }
   
   return 'Workout 2 (' + wod2.length + ' submissions):\n' +
-         '  Team Average: ' + Math.round(+wod2Total / +wod2.length) + ' reps\n' +
-         '  Athlete Average: ' + Math.round((+wod2Total / +wod2.length) / 3) + ' reps\n' +
-         '  Team Median: ' + Math.round(median(wod2)) + '\n' +
+         '  Team Average: ' + average(wod2) + ' reps\n' +
+         '  Athlete Average: ' + athleteAverage(wod2) + ' reps\n' +
+         '  Team Median: ' + median(wod2) + '\n' +
          '  Team High: ' + wod2[0] + '\n' +
          '  Team Low: ' + wod2[wod2.length - 1] + '\n\n';
 }
@@ -143,26 +133,34 @@ function calculateWod3() {
   wod3.sort(function(a, b) {
     return a - b;
   }).reverse();
-
-  var wod3Total = 0;
-  for(var i = 0; i < wod3.length; ++i) {
-    wod3Total += +wod3[i];
-  }
   
   return 'Workout 3 (' + wod3.length + ' submissions):\n' +
-         '  Team Average: ' + Math.round(+wod3Total / +wod3.length) + ' reps\n' +
-         '  Athlete Average: ' + Math.round((+wod3Total / +wod3.length) / 3) + ' reps\n' +
-         '  Team Median: ' + Math.round(median(wod3)) + '\n' +
+         '  Team Average: ' + average(wod3) + ' reps\n' +
+         '  Athlete Average: ' + athleteAverage(wod3) + ' reps\n' +
+         '  Team Median: ' + median(wod3) + '\n' +
          '  Team High: ' + wod3[0] + '\n' +
          '  Team Low: ' + wod3[wod3.length - 1] + '\n\n';
+}
+
+function average(values) {
+  var total = 0;
+  for(var i = 0; i < values.length; ++i) {
+    total += +values[i];
+  }
+  
+  return Math.round(+total / +values.length);
+}
+
+function athleteAverage(values) {
+  return Math.round(average(wod3) / 3);
 }
 
 function median(values) {
   var half = Math.floor(values.length / 2);
 
   if(values.length % 2) {
-    return values[half];
+    return Math.round(values[half]);
   } else {
-    return (values[half - 1] + values[half]) / 2.0;
+    return Math.round((values[half - 1] + values[half]) / 2.0);
   }
 }
