58917e1053fb17073537aba9f603c5b8d6939d0a
Patrick O'Brien <obrienp@outlook.com>
2017-07-12 23:32:18 -0500

    hide button while calculating

--- client.js
+++ client.js
@@ -4,14 +4,17 @@ function onSelectChange() {
 
 function calculate() {
   $('#result').html('');
+  $('#calculate').hide();
 
   $.ajax({
     url: '/calculate/' + $('#division-selector').val(),
     type: 'GET',
     success: function(data) {
+      $('#calculate').show();
       $('#result').html(data.replace(/\n/g, "<br />"));
     },
     error: function(xhr, status, error) {
+      $('#calculate').show();
       console.log('error');
     }
   });
